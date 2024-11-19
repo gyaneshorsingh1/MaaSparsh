2d7cc046bb796f0f5dd7e5805f268817ecacc707
gyaneshorsingh1 <gyaneshorsingh9.com>
2024-11-20 03:44:54 +0530

    build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './dist', // Ensure this points to your build directory
+  },
   plugins: [react()],
   server: {
     proxy: {
