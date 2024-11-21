96682d5ee3e3aa6738d5aa55e05b817ae10b60ee
gyaneshorsingh1 <gyaneshorsingh9.com>
2024-11-21 23:12:03 +0530

    footer desgin setup

--- frontend/src/components/layout/Footer/Footer.jsx
+++ frontend/src/components/layout/Footer/Footer.jsx
@@ -5,85 +5,123 @@ import {
   FaTwitter,
   FaInstagram,
   FaPinterest,
+  FaGooglePay,
+  FaPhone,
+  FaYoutube,
 } from "react-icons/fa";
 import "./Footer.css"; // Import the CSS file
 import logo from "../../../images/logo-without-text.png"
 import Search from "../../Product/Search";
+import footerBanner from "../../../images/footerbanner.png";
+import Cicon from "../../../images/Cicon.png";
+import Ricon from "../../../images/Ricon.png";
+import TMicon from "../../../images/TMicon.png";
+import gpay from "../../../images/gpay.png";
+import phonepay from "../../../images/phonepay.png";
+import paybox from "../../../images/paybox.png";
+import instaicon from "../../../images/insta-icon.png";
+import leaf from "../../../images/leaf.png";
 const Footer = () => {
   return (
+    <>
     <footer>
       <div className="footer-container">
-        <div className="footer-links">
-          <h3>Quick Tabs</h3>
-          <Link to="/about">About Us</Link>
-          <Link to="/contact">Contact Us</Link>
-          <Link to="/privacy">Privacy Policy</Link>
-          <Link to="/terms">Terms of Service</Link>
+      <div className="footer-links">
+          <h3>Our Products</h3>
+          <Link to="/about">All products</Link>
+          <Link to="/contact">Giftings</Link>
+          <Link to="/privacy">Our exclusives</Link>
+          <Link to="/hair-care">Hair Care</Link>
+          <Link to="/body-care">Body Care</Link>
+          <Link to="/demos">Demos</Link>
+          <Link to="/blogs">Blogs</Link>
+
         </div>
+
+        <div className="footer-links">
+          <h3>Our Story</h3>
+          <Link to="/about">Our Philosophy</Link>
+          <Link to="/contact">Social Responsibility</Link>
+          <Link to="/privacy">Media & Press</Link>
+          <Link to="/terms">Policies</Link>
+          <Link to="/terms">Terms</Link>
+          <Link to="/terms">FAQs</Link>
+          <Link to="/terms">MaSparsh Club FAQs</Link>
+        </div>
+        <div className="footer-policies">
+          <h3>Customer Services</h3>
+          <Link to="/policies/shipping-policy">Shipping policy</Link>
+          <Link to="/policies/return-refund-policy">Return Policy</Link>
+          <Link to="/policies/privacy-policy">Privacy Policy</Link>
+          <Link to="/policies/terms-of-service">Terms & Conditions</Link>
+          <Link to="/policies/track-order">Track Order</Link>
+          <Link to="/policies/contact-us">Contact Us</Link>
+          <Link to="/policies/store">Stores</Link>
+          <div className="curtomer-services-c-r-tm-icons">
+            <img src={Cicon} alt="masparsh-copyrights" />
+            <img src={Ricon} alt="masparsh-right" />
+            <img src={TMicon} alt="masparsh-india" />
+          </div>
+        </div>
+
+
         <div className="social-media">
-          <h3>Media</h3>
-          <p>
+          <h3>Contact</h3>
+          <p className="contact-p">
             <b>info@maasparsh.in</b>
-            <p>+91-9999999999</p>
+            <p className="contact-p">+91-9999999999</p>
           </p>
           <p className="img-p"><img src={logo} alt="masparsha logo" /></p>
-          <p>Maasparash Essence of Ayurveda</p>
+          <h4 className="contact-payment">SECURE PAYMENTS</h4>
+          <div className="payicons">
+            <img className="pay-icon" src={gpay} alt="masparsh-gpay" />
+            <img className="pay-icon" src={phonepay} alt="masparsh-phonepay" />
+            <img className="pay-icon" src={paybox} alt="masparsh-gpay" />
+          </div>
           
+        </div>
+        
+        <div className="search-footer">
           <div>
+            <h3 className="footer-offer">YES, I WANT TO HEAR ABOUT SPECIAL<br/> OFFERS AND INSIDER PEAKS</h3>
+          </div>
+          <div className="footer-search">
+              <Search className="search-field" />
+          </div>
+          <h4 className="stay-in-touche">STAY IN TOUCH</h4>
+          <div>
+          <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {/* <FaInstagram className="social-icon" color="rgb(204, 63, 120)" /> */}
+              <img className="insta-icon social-icon" src={instaicon} alt="masparsh insta img" />
+            </a>
             <a
               href="https://www.facebook.com"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FaFacebook className="social-icon" />
+              <FaFacebook className="social-icon" color="blue" />
             </a>
             <a
-              href="https://www.twitter.com"
+              href="https://www.youtube.com"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FaTwitter className="social-icon" />
+              <FaYoutube className="social-icon youtube-icon" color="red"/>
             </a>
-            <a
-              href="https://www.instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaInstagram className="social-icon" />
-            </a>
-            <a
-              href="https://www.pinterest.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaPinterest className="social-icon" />
-            </a>
-          </div>
-        </div>
-        <div className="footer-policies">
-          <h3>Customer Services</h3>
-          <Link to="/policies">All Policies</Link>
-          {/* <Link to="/policies/shipping-policy">Shipping policy</Link>
-          <Link to="/policies/cancellation-policy">Cancellation Policy</Link>
-          <Link to="/policies/return-refund-policy">Return & Refund Policy</Link>
-          <Link to="/policies/privacy-policy">Privacy Policy</Link> */}
-          <Link to="/policies/terms-of-service">Terms & Conditions</Link>
-          <Link to="/policies/track-order">Track Your Order</Link>
-          <Link to="/policies/payment-options">Payment Options</Link>
-          <Link to="/policies/out-of-stock">Out of Stock Items</Link>
-          <Link to="/policies/contact-us">Contact Us</Link>
-        </div>
-        <div className="search-footer">
-          <div>
-            <h3>YES, I WANT TO HEAR ABOUT SPECIAL<br/> OFFERS AND INSIDER PEAKS</h3>
-          </div>
-          <div className="footer-search">
-              <Search className="search-field" />
+
           </div>
 
         </div>
       </div>
+      <img src={leaf} alt="masparsh-leaf" className="footer-leaf" />
+      
     </footer>
+    <img src={footerBanner} alt="footerbanner" className="footer-img" />
+    </>
   );
 };
 
