63b3b935830f2315d3e6e846d37ffb0f1860cf94
Gyaneshor Prasad Singh <118050582+gyaneshorsingh1@users.noreply.github.com>
2025-03-11 14:04:50 +0530

    Update paymentModel.js name

--- backend/models/paymentModel.js
+++ backend/models/paymentModel.js
@@ -13,6 +13,9 @@ const paymentSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    name:{
+        type: String,
+    }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Payment", paymentSchema);
