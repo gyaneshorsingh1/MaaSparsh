1e8146bbbe5de32c6041defce25f9e63230a100e
Gyaneshor Prasad Singh <118050582+gyaneshorsingh1@users.noreply.github.com>
2025-03-12 10:56:57 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
     password:{
         type:String,
         required:[true,"Please Enter Your Password"],
-        minLength:[4,"Password should be greater than 4 characters"],
+        minLength:[8,"Password should be greater than 8 characters"],
         select: false
 
 
