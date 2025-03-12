98ca06b52b3af0c358d4483e51d8ffa79df9258d
Gyaneshor Prasad Singh <118050582+gyaneshorsingh1@users.noreply.github.com>
2025-03-12 11:22:54 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
    methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "x-razorpay-signature"]
 }));
