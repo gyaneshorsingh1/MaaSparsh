cbe8b7f1d037b1a247e7dff594960962850cfdeb
Gyaneshor Prasad Singh <118050582+gyaneshorsingh1@users.noreply.github.com>
2025-03-16 15:33:19 +0530

    Update auth.js tokenn

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
-   const { token } = req.cookies || req.headers.authorization?.split(" ")[1] || Cookies.get('token'); // Ensure you are using cookies
+   const { token } = req.cookies;
 
     if (!token) {
         return next(new ErrorHandler("Please Login to access this resource", 401));
