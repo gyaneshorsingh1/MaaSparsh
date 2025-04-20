989ca1ee5c47f2249d661304ac35dbcd9c99bd6f
gyaneshorsingh1 <gyaneshorsingh9.com>
2025-04-20 22:02:07 +0530

    include the packages file

--- frontend/src/setupProxy.jsx
+++ frontend/src/setupProxy.jsx
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://masparsh.onrender.com/',
+      target: 'https://maasparsh.com/',
       changeOrigin: true,
     })
   );
